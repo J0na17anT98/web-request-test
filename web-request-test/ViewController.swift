60c7b6e88e0501ada8431f5a9f1c0f6c40a670b7
J0na17anT98 <pilotj@outlook.com>
2016-05-17 11:17:41 -0400

    loading webpages app
    created a web view to load 3 pages

--- web-request-test/ViewController.swift
+++ web-request-test/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        webView = WKWebView()
+        container.addSubview(webView)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://developer.apple.com/swift/blog/")
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func loadRequest(urlStr: String) {
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
 
+    @IBAction func loadSwift(sender: AnyObject) {
+        loadRequest("https://developer.apple.com/swift/blog/")
+
+    }
+    
+    @IBAction func TechNews(sender: AnyObject) {
+        loadRequest("http://www.makeuseof.com/tag/google-flash-default-netflix-bad-health-tech-news-digest/")
+
+    }
+    
+    @IBAction func loadSteak(sender: AnyObject) {
+        loadRequest("http://www.goodreads.com/quotes/tag/steak")
+
+    }
 
 }
+
+
 
